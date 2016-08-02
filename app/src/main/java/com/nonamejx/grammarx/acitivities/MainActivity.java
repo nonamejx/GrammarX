119fccf567f5d5ec02bbb8e2a6747265d6f9c984
haihb <huynhbahai94@gmail.com>
2016-08-02 16:15:40 +0700

    Update MainActivity

--- app/src/main/java/com/nonamejx/grammarx/acitivities/MainActivity.java
+++ app/src/main/java/com/nonamejx/grammarx/acitivities/MainActivity.java
@@ -2,9 +2,18 @@ package com.nonamejx.grammarx.acitivities;
 
 import android.support.v4.app.Fragment;
 
+import com.nonamejx.grammarx.R;
 import com.nonamejx.grammarx.fragments.LevelFragment;
 
-public class MainActivity extends SingleFragmentActivity {
+import org.androidannotations.annotations.EActivity;
+
+@EActivity(R.layout.activity_container)
+public class MainActivity extends BaseActivity {
+
+    @Override
+    public void afterView() {
+    }
+
     @Override
     protected Fragment createFragment() {
         return LevelFragment.newInstance();
